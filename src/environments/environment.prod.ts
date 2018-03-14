dd461eca04e4bdcb859beff31c2997304dd467de
kraynzejuarez <kathyrynejuarez@gmail.com>
2018-03-14 19:17:59 +0800

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,1 +1,11 @@
-
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyBYJkqR6-uv4aUl7VaJx3bkutNW0vQweoI",
+        authDomain: "finalexam-1d50c.firebaseapp.com",
+        databaseURL: "https://finalexam-1d50c.firebaseio.com",
+        projectId: "finalexam-1d50c",
+        storageBucket: "finalexam-1d50c.appspot.com",
+        messagingSenderId: "52253064789"
+    }
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: 'APIKEY',
-        authDomain: 'DEV-APP.firebaseapp.com',
-        databaseURL: 'https://DEV-APP.firebaseio.com',
-        projectId: 'DEV-APP',
-        storageBucket: 'DEV-APP.appspot.com',
-        messagingSenderId: '123456789'
+        apiKey: "AIzaSyBYJkqR6-uv4aUl7VaJx3bkutNW0vQweoI",
+        authDomain: "finalexam-1d50c.firebaseapp.com",
+        databaseURL: "https://finalexam-1d50c.firebaseio.com",
+        projectId: "finalexam-1d50c",
+        storageBucket: "finalexam-1d50c.appspot.com",
+        messagingSenderId: "52253064789"
     }
 };
